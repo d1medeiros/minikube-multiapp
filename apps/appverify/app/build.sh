b4eb6c4a00aa65d487543fa5e10c62292d2105e9
diego medeiros <djbmbb@hotmail.com>
2022-09-28 08:05:39 -0300

    refactor

--- apps/appverify/app/build.sh
+++ apps/appverify/app/build.sh
@@ -3,11 +3,11 @@
 #OLDVERSION=$(($OLDVERSION+1))
 #echo $OLDVERSION
 #sed -i 's//d/'$OLDVERSION'/' appversion
-APPNAME="app-puc"
-VERSION=3
+APPNAME="app-verify"
+VERSION=4
 DOCKER_IMAGE="${APPNAME}:${VERSION}"
 echo $DOCKER_IMAGE
-GOOS=linux GOARCH=amd64 go build -ldflags "-extldflags '-static'" -o app ./app.go
+GOOS=linux GOARCH=amd64 go build -ldflags "-extldflags '-static'" -o appbin ./app.go
 export DOCKER_IMAGE=$DOCKER_IMAGE
 echo ${DOCKER_IMAGE}
 docker build -t ${DOCKER_IMAGE} .
